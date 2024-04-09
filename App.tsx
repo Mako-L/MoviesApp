39701bb640756a0b970be54d05d551c111396a72
Mako-L <macovei.gabriel.razvan@gmail.com>
2024-04-10 00:32:18 +0300

    Revert "Updated Readme.md + some code refactoring"
    This reverts commit 4da176066ff60d40f343da933616972e300ee8b0.

--- App.tsx
+++ App.tsx
@@ -1,21 +1,16 @@
-// Import React and necessary components
 import React from 'react';
-import { Provider } from 'react-redux'; // Redux Provider to pass the Redux store down through the component tree
-import { StatusBar } from 'react-native'; // Component to control the app status bar
-import store from './src/store'; // Import the Redux store configuration
-import AppNavigator from './src/navigation/AppNavigator'; // Import the navigation stack
+import { Provider } from 'react-redux';
+import { StatusBar } from 'react-native';
+import store from './src/store';
+import AppNavigator from './src/navigation/AppNavigator';
 
-// Define the main App component as a functional component
 const App: React.FC = () => {
-  return (
-    // Wrap everything inside the Provider to make the Redux store available to all nested components
-    <Provider store={store}>
-      {/* StatusBar component to customize the status bar's appearance */}
-      <StatusBar barStyle="light-content" backgroundColor="#1c1e27" translucent />
-      {/* AppNavigator which contains all the navigation logic and screens of the app */}
-      <AppNavigator />
-    </Provider>
-  );
+    return (
+        <Provider store={store}>
+                 <StatusBar barStyle="light-content" backgroundColor="#1c1e27" translucent />
+            <AppNavigator />
+        </Provider>
+    );
 };
 
 export default App;
